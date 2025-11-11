f79a3f91731ea060e503f70bb852c515d4c1a5a1
Tanishka sawant <tanarjsaw3@gmail.com>
2025-11-11 16:27:13 +0530

    Header changes

---
+++ script.js
@@ -0,0 +1,7 @@
+function openNav() {
+  document.querySelector(".side-nav").classList.add("active");
+}
+
+function closeNav() {
+  document.querySelector(".side-nav").classList.remove("active");
+}
