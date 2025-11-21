934998a60eee785ac9d0b271cc045aabf02b19ae
Arsh1748 <arshshaikh1748@gmail.com>
2025-11-21 19:18:32 +0530

    Chatbot widget integration

---
+++ chatbot.js
@@ -0,0 +1,8 @@
+<!-- Chatbot Widget (Chatbase) -->
+<script>
+  window.chatbaseConfig = {
+    chatbotId: "9ZuJX707AJSizZkSN_xMB",
+    domain: "www.chatbase.co"
+  }
+</script>
+<script src="https://www.chatbase.co/embed.min.js" chatbotId="9ZuJX707AJSizZkSN_xMB" defer></script>
